72a183081d49de27f2dea2cfcd7384b0d6608afa
Adrian <adriangaspar@protonmail.com>
2023-03-24 11:48:30 +0100

    Fixed and improved OnboardScreen functionality

--- app/src/main/java/dev/ayameio/breathwork/onboarding/OnboardScreen.kt
+++ app/src/main/java/dev/ayameio/breathwork/onboarding/OnboardScreen.kt
@@ -1,17 +1,20 @@
 package dev.ayameio.breathwork.onboarding
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import dev.ayameio.breathwork.R
 import dev.ayameio.breathwork.ui.theme.BreathworkTheme
 
 
@@ -28,7 +31,8 @@ fun OnboardPreview() {
 fun OnboardScreen(
     modifier: Modifier = Modifier,
 ) {
-    var text: String by remember { mutableStateOf("") }
+    var nameValue: String by remember { mutableStateOf("") }
+    var emailValue: String by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -47,49 +51,61 @@ fun OnboardScreen(
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            OutlinedTextField(
-                modifier = modifier,
-                shape = CircleShape,
-                label = {
-                    Text(text = "Name")
-                },
-                value = text,
-                onValueChange = { text = it },
-                colors = TextFieldDefaults.textFieldColors(
-                    backgroundColor = MaterialTheme.colors.background
-                )
+            EditField(
+                value = nameValue,
+                onValueChange = { nameValue = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Ascii),
+                id = R.string.name
             )
-            OutlinedTextField(
-                modifier = modifier,
-                shape = CircleShape,
-                label = {
-                    Text(text = "Email")
-                },
-                value = text,
-                onValueChange = { text = it },
-                colors = TextFieldDefaults.textFieldColors(
-                    backgroundColor = MaterialTheme.colors.background
-                )
+            EditField(
+                value = emailValue,
+                onValueChange = { emailValue = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                id = R.string.email
             )
-            Button(
-                modifier = Modifier
-                    .padding(top = 36.dp)
-                    .height(TextFieldDefaults.MinHeight)
-                    .width(TextFieldDefaults.MinWidth),
-                onClick = { /* ... */ },
-                // Uses ButtonDefaults.ContentPadding by default
-
-                shape = CircleShape,
-            ) {
-                // Inner content including an icon and a text label
-                Text("Join")
-                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                Icon(
-                    Icons.Filled.ArrowForward,
-                    contentDescription = "Join",
-                    modifier = Modifier.size(ButtonDefaults.IconSize)
-                )
-            }
+            JoinButton()
         }
+    }
+}
+
+@Composable
+fun EditField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
+    @StringRes id: Int,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        keyboardOptions = keyboardOptions,
+        singleLine = true,
+        label = {
+            Text(text = stringResource(id = id))
+        },
+        shape = CircleShape,
+    )
+}
+
+@Composable
+fun JoinButton() {
+    Button(
+        modifier = Modifier
+            .padding(top = 36.dp)
+            .height(TextFieldDefaults.MinHeight)
+            .width(TextFieldDefaults.MinWidth),
+        onClick = { /* ... */ },
+        // Uses ButtonDefaults.ContentPadding by default
+
+        shape = CircleShape,
+    ) {
+        // Inner content including an icon and a text label
+        Text(text = stringResource(id = R.string.join_for_free))
+        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+        Icon(
+            Icons.Filled.ArrowForward,
+            contentDescription = "Join",
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
     }
 }
